811a42fe644edfefd51c5bcb1e994b463709d3f3
geadalfa <ggeadalfa@gmail.com>
2021-11-19 14:35:31 +0700

    Hasil1

--- tes.py
+++ tes.py
@@ -1,16 +1,16 @@
 def Iter(num):
     tmpData = []
-    for i in range(num+1):
-        if i>0:
-            if i%2 == 0 or i%5==0 and i%3==0: 
-                tmpData.append(str(i))
-            else:
-                tmpData.append(str(i))
-            if i%3==0:
-                tmpData.append('Frontend')
+    for i in range(1,num+1):
 
-            if i%5==0:
-                tmpData.append('Backend')
+        if i%3==0 :
+            tmpData.append('Frontend')
+        elif i%5==0:
+            tmpData.append('Backend')
+        else:
+            tmpData.append(str(i))
+        if i%5==0 and i%3==0 :
+            tmpData.append('Backend')
+            
 
     print(','.join(tmpData))
 
