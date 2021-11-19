6b4eba419b947175aa9d83eee42cff391b0fd060
geadalfa <ggeadalfa@gmail.com>
2021-11-19 13:56:55 +0700

    Hasil1

--- tes.py
+++ tes.py
@@ -1,1 +1,17 @@
-print('Hello Alfa')
+def Iter(num):
+    tmpData = []
+    for i in range(num+1):
+        if i>0:
+            if i%2 == 0 or i%5==0 and i%3==0: 
+                tmpData.append(str(i))
+            else:
+                tmpData.append(str(i))
+            if i%3==0:
+                tmpData.append('Frontend')
+
+            if i%5==0:
+                tmpData.append('Backend')
+
+    print(','.join(tmpData))
+
+Iter(50)
